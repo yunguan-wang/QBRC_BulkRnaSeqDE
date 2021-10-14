eb32b0dd1ab5bcf828e9c1f602169cb55b1f79f5
yunguan-wang <drizzter.yw@gmail.com>
2021-10-14 11:04:04 -0500

    updates with WGCNA

--- DE.r
+++ DE.r
@@ -39,6 +39,9 @@
 #   --ssgsea, -sg         
 #     Toggle to run ssGSEA for all samples. Default FALSE
 
+#   --wgcna, -w         
+#     Toggle to run WGCNA for all samples. Default FALSE
+
 # Try this:
 # Rscript DE.r ./example_data/example_expression.txt ./example_data/example_group.txt -o ./results 
 #   -r /project/shared/xiao_wang/software/rnaseqDE/example_data/h.all.v7.0.symbols.gmt -g T
@@ -47,12 +50,12 @@
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
-packages <- c("DESeq2","fgsea",'fastmatch','pheatmap','GSVA')
+packages <- c("DESeq2","fgsea",'fastmatch','pheatmap','GSVA', 'WGCNA', "HybridMTest")
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   BiocManager::install(setdiff(packages, rownames(installed.packages())))
 }
 
-packages <- c("ggplot2", "dplyr",'tibble','argparse')
+packages <- c("ggplot2", "dplyr",'tibble','argparse', 'readr', 'gprofiler2')
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
   install.packages(setdiff(packages, rownames(installed.packages())))  
 }
@@ -66,6 +69,7 @@ suppressMessages(library('tibble'))
 suppressMessages(library('pheatmap'))
 suppressMessages(library('argparse'))
 suppressMessages(library('GSVA'))
+suppressMessages(library('gprofiler2'))
 
 # debug testing for using in Rstudio
 # setwd('/project/shared/xiao_wang/software/rnaseqDE/')
@@ -75,7 +79,7 @@ suppressMessages(library('GSVA'))
 # gsea_ref = "/project/shared/xiao_wang/software/rnaseqDE/example_data/h.all.v7.0.symbols.gmt"
 # fc_cutoff <- 1
 # pval_cutoff <- 0.05
-# v_pval_cutoff <- 0.05
+# v_pval_cutoff <- 0.001
 # species <- 'human'
 # partialheatmap <- T
 # clustercol <- T
@@ -136,6 +140,10 @@ parser$add_argument(
     '--ssgsea', '-sg', action = 'store_true', default = FALSE,
     help = 'Toggle to run ssGSEA for all samples. Default FALSE')
 
+parser$add_argument(
+    '--wgcna', '-w', action = 'store_true', default = FALSE,
+    help = 'Toggle to run WGCNA for all samples. Default FALSE')
+
 args <- parser$parse_args()
 cts <- args$counts_fn
 design <- args$design_fn
@@ -150,6 +158,7 @@ species <- args$species
 partialheatmap <- args$partialheatmap
 clustercol <- args$nocolclustering
 ssgsea <- args$ssgsea
+wgcna = args$wgcna
 
 # --------
 # Preprocessing input data
@@ -207,11 +216,13 @@ dds <- nbinomWaldTest(dds)
 vsd <- vst(dds, blind=FALSE)
 pcaData <- plotPCA(vsd, intgroup=c("condition"), returnData=TRUE)
 percentVar <- round(100 * attr(pcaData, "percentVar"))
+pcaData['Name'] = row.names(pcaData)
 ggplot(
   pcaData, aes(PC1, PC2, color=condition)) +
   geom_point(size=3) +
   xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-  ylab(paste0("PC2: ",percentVar[2],"% variance"))
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
+  geom_text_repel(aes(label = Name), size = 4)
 ggsave('PCA_plot.pdf',height = 5, width = 5)
 
 # Distance plot
@@ -255,21 +266,21 @@ for (c in analysis){
   
   # Volcano plot
   data2voc = res[!is.na(res$padj),]
-  data2voc$sig = ifelse(
-    (abs(data2voc$log2FoldChange)>=fc_cutoff) & (data2voc$padj<=v_pval_cutoff),
-    "Significant", "Non-significant")
+    data2voc$sig = ifelse(
+        (abs(data2voc$log2FoldChange)>=fc_cutoff) & (data2voc$padj<=v_pval_cutoff),
+        "Significant Up", "Non-significant")
+    data2voc[
+        (data2voc$sig == "Significant Up") & (data2voc$log2FoldChange<=-fc_cutoff),"sig"
+        ] = "Significant Down"
   data2voc = as.data.frame(data2voc)
   data2voc$padj = -log10(data2voc$padj)
-  cols=c("Significant"='darkolivegreen4',"Non-significant"='grey')
+  cols=c("Significant Up"='red', "Significant Down"='blue',"Non-significant"='grey')
   # Capping logFC magnitude to 10
   data2voc$log2FoldChange = ifelse(data2voc$log2FoldChange > 10, 10, data2voc$log2FoldChange)
   data2voc$log2FoldChange = ifelse(data2voc$log2FoldChange < -10, -10, data2voc$log2FoldChange)
   
-  data2voc$padj = ifelse(data2voc$padj > 10, 10, data2voc$padj)
-  data2voc$padj = ifelse(data2voc$padj < -10, -10, data2voc$padj)
-  
   data2voc = data2voc[order(abs(data2voc$log2FoldChange),decreasing = T),]
-  keep =(row.names(data2voc)[data2voc$sig=='Significant'])[1:20]
+  keep = row.names(data2voc)[data2voc$sig %in% c('Significant Up','Significant Down')][1:20]
   keep = keep[!is.na(keep)]
   data2voc$Gene = NA
   data2voc[keep,"Gene"] = keep
@@ -378,3 +389,129 @@ for (c in analysis){
 }
 
 warnings() # by wtwt5237
+
+# WGCNA
+if (wgcna == T) {
+  # 0. Setup
+  suppressMessages(library('WGCNA'))
+  datExpr = data.frame(t(export_counts))
+  names(datExpr) = row.names(export_counts)
+  dimnames(datExpr)[[1]] = names(data.frame(row.names(export_counts)))
+  gsg = goodSamplesGenes(datExpr, verbose = 3)
+  dir.create("WGCNA_Results")
+
+  # 1. Scale Independence & Mean Connectivity
+  powers = c(c(1:10), seq(from = 12, to=20, by=2))
+  sft = pickSoftThreshold(datExpr, powerVector = powers, verbose = 5)
+  sizeGrWindow(9, 5)
+  par(mfrow = c(1,2));
+  cex1 = 0.9;
+  pdf(file = "WGCNA_Results/Scale Independence and Mean Connectivity.pdf")
+  plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+      xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",
+      main = paste("Scale Independence"));
+  text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+      labels=powers,cex=cex1,col="blue");
+  abline(h=0.90,col="blue")
+
+  plot(sft$fitIndices[,1], sft$fitIndices[,5],
+      xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
+      main = paste("Mean Connectivity"))
+  text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="blue")
+  dev.off()
+
+  # 2. Cluster Dendrogram
+  ysft <- -sign(sft$fitIndices[,3])*sft$fitIndices[,2]
+  best_power = Position(function(x) x > 0.9, ysft)
+
+  cor <- WGCNA::cor
+  datExpr[] <- lapply(datExpr, as.numeric)
+  net = blockwiseModules(datExpr, power = best_power,
+                        TOMType = "unsigned", minModuleSize = 30,
+                        reassignThreshold = 0.01, mergeCutHeight = 0.1,
+                        numericLabels = TRUE, pamRespectsDendro = FALSE,
+                        verbose = 3, nThreads = 16, deepSplit = 4)
+  moduleLabels = net$colors
+  moduleColors = labels2colors(net$colors)
+  MEs = net$MEs;
+  geneTree = net$dendrograms[[1]];
+  cor<-stats::cor
+
+  sizeGrWindow(12, 9)
+  mergedColors = labels2colors(net$colors)
+  pdf(file = "WGCNA_Results/Cluster Dendrogram.pdf")
+  plotDendroAndColors(net$dendrograms[[1]], mergedColors[net$blockGenes[[1]]],
+                      "Module colors",
+                      dendroLabels = FALSE, hang = 0.03,
+                      addGuide = TRUE, guideHang = 0.05)
+  dev.off()
+
+  # 3. Average Expression Heatmap
+  # reference <- table(net$colors)
+  avgexp <- moduleEigengenes(datExpr, moduleColors)$averageExpr
+  avgexp <- t(avgexp)
+  avgexp = avgexp[,row.names((design))]
+
+  suppressMessages(library('HybridMTest'))
+  anova_res = row.oneway.anova(avgexp,design$Group)
+  keep = anova_res$pval<=0.01
+  avgexp = avgexp[keep,]
+
+  pheatmap(
+    avgexp, height = 16, fontsize = 8, cluster_cols = F,
+    annotation_col = subset(design,select = 'Group'),
+    scale='column',
+    file = "WGCNA_Results/Average Expression Heatmap.pdf",
+  )
+  # 4. Output significant modules list
+  sig_modules = row.names(avgexp)
+  sig_modules = substring(sig_modules, first = 3, last = 99)
+  geneInfo0 = data.frame(
+    Genes = names(datExpr), 
+    moduleLabel = moduleLabels, moduleColor = moduleColors)
+  geneOrder = order(geneInfo0$moduleColor)
+  geneInfo = geneInfo0[geneOrder, ]
+  write.csv(geneInfo, "WGCNA_Results/geneInfo.csv")
+
+  # 5. Significant module heatmap
+  for (m in sig_modules) {
+    genes = row.names(geneInfo[geneInfo$moduleColor == m,])
+    heatmap_mats <- export_counts[genes, ]
+    heatmap_mats <- na.omit(heatmap_mats)
+    pheatmap(
+      heatmap_mats, height = 16, fontsize = 5, 
+      annotation_col = subset(design,select = 'Group'), cluster_cols = F,
+      filename = paste("WGCNA_Results/Module_Heatmaps/", m, "_heatmap.pdf",sep = ''),
+    )
+  }
+
+  # 6. Eigengene Heatmap & Dendrogram
+  sig_modules <- paste0("ME", sig_modules)
+  MEs = moduleEigengenes(datExpr, moduleColors)$eigengenes
+  MET = orderMEs(MEs)
+  cMET <- colnames(MET)
+  cmatch <- match(sig_modules, cMET)
+  sigMET <- MET[, cmatch]
+
+  sizeGrWindow(5,7.5);
+  par(cex = 0.9)
+  pdf(file = "WGCNA_Results/Eigengene Adjacency Heatmap.pdf")
+  plotEigengeneNetworks(
+    sigMET, "Eigengene Adjacency Heatmap", marDendro = c(0,4,1,2), 
+    marHeatmap = c(3,4,1,2), cex.lab = 0.8, xLabelsAngle = 90)
+  dev.off()
+
+  # 7. Functional Enrichment for Significant Modules
+  xli <- names(datExpr)
+  genelist <- gconvert(xli, organism = "hsapiens", target = "ENTREZGENE_ACC", filter_na = T)
+  genelist <- genelist[,!(names(genelist) %in% c("input_number", "target_number", "name", "description", "namespace"))]
+  genelist <- genelist[- grep("nan", genelist$target),]
+  entrezid <- genelist$target
+
+  GOenr = GOenrichmentAnalysis(moduleColors, entrezid, organism = "human", nBestP = 10)
+  tab = GOenr$bestPTerms[[4]]$enrichment
+
+  sig_modules1 = sub("ME", "", sig_modules)
+  sigtab <- tab[tab$module %in% sig_modules1, ]
+  write.table(sigtab, file = "WGCNA_Results/GOEnrichmentTable.csv", sep = ",", quote = TRUE, row.names = FALSE)
+}
